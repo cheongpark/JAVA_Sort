97e12512d1f0732d4f0dc4e171df7dcd3330f304
Cheongpark(청박) <55844749+cheongpark@users.noreply.github.com>
2022-11-22 10:58:26 +0900

    Rename * Move

--- src/sort/insert.java
+++ src/sort/insert.java
@@ -2,16 +2,7 @@ package sort;
 
 import java.util.Scanner;
 
-public class insert {
-    static void insertionSort(int[] a) {
-        for(int i = 1; i < a.length; i++) {
-            int j, tmp = a[i];
-            for(j = i; j > 0 && a[j - 1] > tmp; j--)
-                a[j] = a[j - 1];
-            a[j] = tmp;
-        }
-    }
-
+public class Insert {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
         int n = stdin.nextInt();
@@ -24,5 +15,14 @@ public class insert {
 
         for(int i = 0; i < n; i++)
             System.out.println(data[i]);
+    }
+
+    static void insertionSort(int[] a) {
+        for(int i = 1; i < a.length; i++) {
+            int j, tmp = a[i];
+            for(j = i; j > 0 && a[j - 1] > tmp; j--)
+                a[j] = a[j - 1];
+            a[j] = tmp;
+        }
     }
 }
--- src/sort/shell.java
+++ src/sort/shell.java
@@ -2,17 +2,7 @@ package sort;
 
 import java.util.Scanner;
 
-public class shell {
-    static void shellSort(int[] a) {
-        for(int h = a.length / 2; h > 0; h /= 2)
-            for(int i = h; i < a.length; i++) {
-                int j, tmp = a[i];
-                for(j = i - h; j >= 0 && a[j] > tmp; j -= h)
-                    a[j + h] = a[j];
-                a[j + h] = tmp;
-            }
-    }
-
+public class Shell {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
         int n = stdin.nextInt();
@@ -25,5 +15,15 @@ public class shell {
 
         for(int i = 0; i < n; i++)
             System.out.println(data[i]);
+    }
+
+    static void shellSort(int[] a) {
+        for (int h = a.length / 2; h > 0; h /= 2)
+            for (int i = h; i < a.length; i++) {
+                int j, tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                    a[j + h] = a[j];
+                a[j + h] = tmp;
+            }
     }
 }
